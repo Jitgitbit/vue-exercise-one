2500df0e8c1518c2df30355c2c246accb8a2c7ae
Jitgitbit <rowowolf@gmail.com>
2020-04-01 15:19:45 +0200

    first Vue method made

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: '#app',
   data: {
-    title:'Becoming a Vue Phoenix'
+    title:'Becoming a Vue Phoenix',
+    name:'Thomas'
+  },
+  methods: {
+    greet(time){
+      return `Hello and good ${time} ${this.name}.`   // Unlike with React, not this.data.name !! Vue goes straight to the top !!
+    }
   }
 })
